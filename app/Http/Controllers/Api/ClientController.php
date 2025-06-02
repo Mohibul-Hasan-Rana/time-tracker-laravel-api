836120a60f1113cdc84c6e0f532d619d520f0136
rana <rana.csesust@gmail.com>
2025-06-02 17:58:10 +0600

    Client completed

--- app/Http/Controllers/Api/ClientController.php
+++ app/Http/Controllers/Api/ClientController.php
@@ -3,9 +3,49 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ClientRequest;
+use App\Models\Client;
 use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class ClientController extends Controller
 {
-    //
+    use AuthorizesRequests;
+
+    public function index(Request $request)
+    {
+        return $request->user()->clients()->get();
+    }
+
+    public function store(ClientRequest $request, Client $client)
+    {
+        $this->authorize('create', $client);
+
+        $client = $request->user()->clients()->create($request->validated());
+        return response()->json($client, 201);
+    }
+
+    public function update(ClientRequest $request, Client $client)
+    {
+        $this->authorize('update', $client);       
+
+        $client->update($request->validated());
+        return response()->json($client);
+    }
+
+    public function destroy(Client $client)
+    {
+        $this->authorize('delete', $client);
+
+        $client->delete();
+        return response()->json(['message' => 'Client deleted']);
+    }
+
+    public function show(Client $client)
+    {
+        $this->authorize('view', $client);
+         return response()->json($client);
+    }
+
+   
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,24 @@
 
 namespace App\Providers;
 
+use App\Models\Client;
+use App\Models\Project;
+use App\Models\TimeLog;
+use App\Policies\ClientPolicy;
+use App\Policies\ProjectPolicy;
+use App\Policies\TimeLogPolicy;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+
+    protected $policies = [
+        Client::class => ClientPolicy::class,
+        Project::class => ProjectPolicy::class,
+        TimeLog::class => TimeLogPolicy::class,
+    ];
+
+    
     /**
      * Register any application services.
      */
@@ -21,4 +35,6 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
+
+    
 }
